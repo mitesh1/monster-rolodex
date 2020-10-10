cbe194e74527a1501ff0b6e0b9123f9e7e77c39d
mitesh1 <bishtmitesh@gmail.com>
2020-10-11 02:33:59 +0530

    Updates

---
+++ precache-manifest.c865da72852abf0cef85cd4ffd943c46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93efb987621a964261c9e4d39a93155",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "16f2d689b797e91b61b9",
+    "url": "/monster-rolodex/static/css/main.b612df3b.chunk.css"
+  },
+  {
+    "revision": "082f8914de693280636e",
+    "url": "/monster-rolodex/static/js/2.9cbb02df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9cbb02df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f2d689b797e91b61b9",
+    "url": "/monster-rolodex/static/js/main.03ff3f06.chunk.js"
+  },
+  {
+    "revision": "192894fcf2c1737177cd",
+    "url": "/monster-rolodex/static/js/runtime-main.df1e3023.js"
+  }
+]);
